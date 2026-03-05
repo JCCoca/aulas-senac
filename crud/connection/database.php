f4b861c30726b0d2f25f70ec3e224d51ac9f7813
José Coca Carrascosa Neto <josecoca.cn@gmail.com>
2026-03-05 18:01:44 -0500

    Feat: conexão com banco de dados

--- crud/connection/database.php
+++ crud/connection/database.php
@@ -1,9 +1,10 @@
 <?php 
 
 try {
-    $connection = new PDO('mysql:host=localhost;dbname=meu_estoque', 'root', '', [
+    $connection = new PDO('mysql:host=localhost;dbname=meu_estoque;charset=utf8', 'root', '', [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, 
+        PDO::ATTR_EMULATE_PREPARES   => false
     ]);
 } catch (PDOException $error) {
     echo "<b>ERRO FATAL NA CONEXÃO</b><br>
