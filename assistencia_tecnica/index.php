ca1bf38dc42faa1876bdd18f0eae85ce60cfcaa7
José Coca Carrascosa Neto <josecoca.cn@gmail.com>
2026-03-17 21:26:30 -0500

    Login

--- assistencia_tecnica/index.php
+++ assistencia_tecnica/index.php
@@ -3,6 +3,8 @@
 require_once 'config/app.php';
 require_once 'connection/database.php';
 
+session_start();
+
 if (isset($_GET['vp']) and !empty($_GET['vp'])) {
     $page = $_GET['vp'];
     require_once 'views/pages/'.$page.'.php';
@@ -12,3 +14,5 @@ if (isset($_GET['vp']) and !empty($_GET['vp'])) {
 } else {
     require_once 'views/pages/home.php';
 }
+
+session_write_close();
---
+++ assistencia_tecnica/views/pages/login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assistência Técnica</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</head>
+<body class="bg-body-secondary">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-lg-5 col-md-6 mx-auto">
+                <h3 class="text-center mb-4">Login</h3>
+
+                <div class="card card-body">
+                    <form action="" method="POST">
+                        <div class="mb-3">
+                            <label for="email" class="form-label">E-mail</label>
+                            <input type="email" name="email" id="email" class="form-control" required>  
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="senha" class="form-label">Senha</label>
+                            <input type="password" name="senha" id="senha" class="form-control" required>  
+                        </div>
+                        
+                        <div class="d-grid gap-2">
+                            <button type="submit" class="btn btn-primary">
+                                Entrar
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
